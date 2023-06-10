52961056792b942911070c64417d34eee62410bc
Nate Butler <iamnbutler@gmail.com>
2023-06-10 11:34:44 -0400

    Add tests for hexToRGB

--- src/color/hexToRGB.ts
+++ src/color/hexToRGB.ts
@@ -1,14 +1,22 @@
 interface RGB {
-    readonly r: number
-    readonly g: number
-    readonly b: number
+    readonly r: number;
+    readonly g: number;
+    readonly b: number;
 }
 
 function hexToRGB(hex: string): RGB {
+    if (!hex.startsWith("#")) {
+        throw new Error(`Hex values must be prefixed with #`);
+    }
+
+    if (hex.length !== 7) {
+        throw new Error(`Invalid hex string ${hex}`);
+    }
+
     const rgb: RGB = {
-        r: parseInt(hex.substring(1, 3), 16) / 255,
-        g: parseInt(hex.substring(3, 5), 16) / 255,
-        b: parseInt(hex.substring(5, 7), 16) / 255,
+        r: Math.round((parseInt(hex.substring(1, 3), 16) / 255) * 100) / 100,
+        g: Math.round((parseInt(hex.substring(3, 5), 16) / 255) * 100) / 100,
+        b: Math.round((parseInt(hex.substring(5, 7), 16) / 255) * 100) / 100,
     };
 
     return rgb;
